b1695b2f67ac617ad3d16c24a5d72ce43358f0bc
Garrett Kim <gkim@gkim-505.local>
2016-06-22 12:26:51 -0700

    initial

---
+++ app/src/main/java/com/isbx/vectordrawableexample/VectorExampleActivity.java
@@ -0,0 +1,14 @@
+package com.isbx.vectordrawableexample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VectorExampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vector_example);
+    }
+}
